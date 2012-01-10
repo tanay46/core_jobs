c385a5b6d8886af11b5df03cdc253115b74865fb
Brian Shin <shindonghee@gmail.com>
2012-01-10 00:18:32 -0500

    Frontend changes

--- core_jobs/static/js/jobs.js
+++ core_jobs/static/js/jobs.js
@@ -1,13 +1,21 @@
-$(function(){
-    $("div.tags a").click(function(){
-        $.get(
-            '/jobs/tag/',
-            {q: $(this).text()},
-            function(data) {
-                $(data).insertAfter(".posts:first")
-                $(".posts:first").animate({ height: 'toggle', opacity: 'toggle' }, "slow", function(){
-                    $(".posts:first").remove();
-                });
+$(function() {
+    $("#subnav-well").height($(window).height() - 180);
+    $(window).resize(function(){
+        $("#subnav-well").height($(window).height() - 180);
+    });
+
+    //Tagcloud functionality
+    $("div.tags a").click(function() {
+        $.get('/jobs/tag/', {
+            q: $(this).text()
+        }, function(data) {
+            $(".posts:first").after(data).animate({
+                height: 'toggle',
+                opacity: 'toggle'
+            }, "slow", function() {
+                $(".posts:first").remove();
+            });
         });
+        return false;
     });
 });
